5d9985041f861f801e8972ba1e45455db9942557
Lucila Viappiani <lucilaviappiani@hotmail.com>
2023-05-07 19:33:41 -0300

    modificando comentarios

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//Definimos la función 
+
 function calcularPrecio(valorProducto, codigoPromocional, cuotas) {
     // Definimos variable para descuento
     let descuento = 0;
@@ -14,41 +17,43 @@ function calcularPrecio(valorProducto, codigoPromocional, cuotas) {
     //precio sin descuento
     let precioBase = valorProducto;
   
-    // Precio con descuento descuento
+    // Precio con descuento
     let precioDescuento = precioBase - (precioBase * descuento);
   
     // Calculamos valor de las cuotas
     let valorCuota = precioDescuento/ cuotas;
 
-      // Si se ingresa más de 3 cuotas, se agrega un interés del 15%
+    // Si se ingresa más de 3 cuotas, se agrega un interés del 15%
     if (cuotas > 3) {
     precioDescuento = precioDescuento * 1.15;
     valorCuota = precioDescuento / cuotas;
     }
   
-    // Devolver precio con descuentos, y valor de las cuotas
+    // Devolvemos precio con descuentos, y valor de las cuotas
    return [precioDescuento, valorCuota];
   }
 
 // Definimos un array vacío para almacenar los productos
 let productos = [];
+
+
 let continuar = true;
 let producto = 1;
   
 while (continuar) {
-    // Solicitar al usuario el precio base del producto. Usamos parseFloat ya que este valor puede tener decimales
+    // Solicitamos al usuario el precio inicial del producto. Usamos parseFloat ya que este valor puede tener decimales
     const precioBase = parseFloat(prompt(`Ingrese el precio base del producto número ${producto}`));
   
-    // Solicitar al usuario el código de descuento
+    // Solicitamos al usuario el código de descuento
     const codigoDescuento = prompt(`Ingrese el código de descuento para el producto ${producto} (DESC10, DESC20 O DESC30)`);
   
-    // Solicitar al usuario la cantidad de cuotas
+    // Solicitamos al usuario la cantidad de cuotas
     const cuotas = parseInt(prompt(`Ingrese la cantidad de cuotas para el producto ${producto} (Hasta 3 cuotas sin interés | 15% INTERÉS EN MÁS DE 3 CUOTAS.)`));
   
-    // Llamar a la función calcularPrecio con los valores ingresados por el usuario
+    // Llamamos a la función calcularPrecio con los valores ingresados por el usuario
     const precioFinal = calcularPrecio(precioBase, codigoDescuento, cuotas);
   
-    // Mostrar el precio final al usuario
+    // Mostramos el precio final al usuario
     alert(`El precio final del producto número ${producto} abonando en ${cuotas} cuotas es de $${precioFinal[0].toFixed(2)}, y el valor de las cuotas es de $${precioFinal[1].toFixed(2)} cada una.`);
 
     productos.push({
@@ -56,12 +61,12 @@ while (continuar) {
         valorCuota: precioFinal[1]
       });
 
-    // Preguntar al usuario si quiere ingresar otro producto
+    // Preguntamos al usuario si quiere ingresar otro producto
     continuar = confirm("¿Desea calcular el precio de otro producto?");
     producto++;
 }
 
-// Mostrar los resultados de todos los productos ingresados
+// Mostramos los resultados de todos los productos ingresados
 let mensaje = "Tus productos:";
 
 for (let i = 0; i < productos.length; i++) {
